0f214c2f09e23bd7e41787f52d1954d1d28db4a6
이주성 <32183520@dankook.ac.kr>
2024-05-27 13:58:22 +0900

    프로젝트 초기 세팅

---
+++ dl-lab/src/main/java/com/example/dllab/DlLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.dllab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlLabApplication.class, args);
+    }
+
+}

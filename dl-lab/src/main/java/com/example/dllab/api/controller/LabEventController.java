a5eda0e7c4e74e4e529029502ca38ea464c2e8f3
이주성 <32183520@dankook.ac.kr>
2024-06-16 02:38:54 +0900

    Update LabEventController.java

--- dl-lab/src/main/java/com/example/dllab/api/controller/LabEventController.java
+++ dl-lab/src/main/java/com/example/dllab/api/controller/LabEventController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/lab-event")
+@RequestMapping("/lab/event")
 @RequiredArgsConstructor
 public class LabEventController {
     private final LabEventService labEventService;
